e998d0be76a2d0053ab34c12686859a83fcc4629
zulkvlt <43006652+zulkvlt@users.noreply.github.com>
2018-09-05 21:46:04 +0700

    Add files via upload
    achmad zukfikar
    1631130110
    TT3A

---
+++ zulfikar/src/zulfikar/Zulfikar.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zulfikar;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Zulfikar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code applicati[on logic here
+    int jumlah = 5;
+            System.out.print("jumlah=");
+            System.out.print(jumlah);
+        for(int a=0;a<=jumlah;a++){
+           for(int b=0;b<a;b++){
+               System.out.print("*");
+           } 
+           System.out.println("");
+    }
+    }
+    
+}
